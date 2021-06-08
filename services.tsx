02be341924d4073a94efd68d8f61a26fc6b68b8a
waynewee <wwayne.wee@gmail.com>
2021-06-09 00:45:49 +0800

    changed api url

--- services.tsx
+++ services.tsx
@@ -2,20 +2,28 @@ import axios from 'axios'
 
 import {PIECE} from './enums'
 
+const production = process.env.NODE_ENV === 'production'
+
+let API_URL = `http://localhost:8000`
+
+if (production) {
+  API_URL = `http://18.117.252.73`
+}
+
 export const _getNewSession = (boardSize: number) => axios({
   method: 'get',
   withCredentials: true,
-  url: `http://localhost:8000/new/${boardSize}`
+  url: `${API_URL}/new/${boardSize}`
 })
 
 export const _getSession = (sessionId: string, playerName?: string) => axios({
   method: 'get',
   withCredentials: true,
-  url: `http://localhost:8000/${sessionId}${playerName?`?playerName=${playerName}`:''}`
+  url: `${API_URL}/${sessionId}${playerName?`?playerName=${playerName}`:''}`
 })
 
 export const _postMove = (sessionId: string, rowIndex: number, colIndex: number, piece: PIECE) => axios({
   method: 'post',
   withCredentials: true,
-  url: `http://localhost:8000/${sessionId}?i=${rowIndex}&j=${colIndex}&piece=${piece}`
+  url: `${API_URL}/${sessionId}?i=${rowIndex}&j=${colIndex}&piece=${piece}`
 })
