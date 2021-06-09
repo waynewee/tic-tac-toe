ef2322e7c95e05e1446df46ad8f1693ff5f43d84
waynewee <wwayne.wee@gmail.com>
2021-06-09 19:35:15 +0800

    fixed win overlay issues

--- components/peripherals/win-overlay.tsx
+++ components/peripherals/win-overlay.tsx
@@ -10,16 +10,17 @@ class WinOverlay extends React.Component<IWinOverlayProps> {
   render() {
     return (
       <>
-        <div className={`win-overlay-background${this.props.isLoseState?' lose-overlay-background':''}`}/>
-        <div className="win-overlay">
-          <div>
-            <div className="win-overlay-name">
-              {this.props.winnerName.toLowerCase()} won!
-            </div>
-            <div 
-            onClick={this.props.onClickMenu}
-            className="default-button default-button-solid">
-              Go To Menu
+        <div className={`win-overlay${this.props.isLoseState?' lose-overlay':''}`}>
+          <div className="win-overlay-content">
+            <div>
+              <div className="win-overlay-name">
+                {this.props.winnerName.toLowerCase()} won!
+              </div>
+              <div 
+              onClick={this.props.onClickMenu}
+              className="default-button default-button-solid">
+                Go To Menu
+              </div>
             </div>
           </div>
         </div>
