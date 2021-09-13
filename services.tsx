bcc85d5cbd6095e39c600fabfc77da4f8f0b4ef4
Wayne Wee <wwayne.wee@gmail.com>
2021-09-13 09:37:32 +0800

    added elastic ip

--- services.tsx
+++ services.tsx
@@ -1,35 +1,46 @@
-import axios from 'axios'
+import axios from "axios";
 
-import {PIECE} from './components/enums'
+import { PIECE } from "./components/enums";
 
-const production = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === "production";
 
-let API_URL = `http://localhost:8000`
+let API_URL = `http://localhost:8000`;
 
 if (production) {
-  API_URL = `http://ec2-18-117-252-73.us-east-2.compute.amazonaws.com`
+  API_URL = `http://3.143.72.165/`;
 }
 
-export const _getNewSession = (boardSize: number) => axios({
-  method: 'get',
-  withCredentials: true,
-  url: `${API_URL}/new/${boardSize}`
-})
+export const _getNewSession = (boardSize: number) =>
+  axios({
+    method: "get",
+    withCredentials: true,
+    url: `${API_URL}/new/${boardSize}`,
+  });
 
-export const _getSession = (sessionId: string, playerName?: string) => axios({
-  method: 'get',
-  withCredentials: true,
-  url: `${API_URL}/${sessionId}${playerName?`?playerName=${playerName}`:''}`
-})
+export const _getSession = (sessionId: string, playerName?: string) =>
+  axios({
+    method: "get",
+    withCredentials: true,
+    url: `${API_URL}/${sessionId}${
+      playerName ? `?playerName=${playerName}` : ""
+    }`,
+  });
 
-export const _postMove = (sessionId: string, rowIndex: number, colIndex: number, piece: PIECE) => axios({
-  method: 'post',
-  withCredentials: true,
-  url: `${API_URL}/${sessionId}?i=${rowIndex}&j=${colIndex}&piece=${piece}`
-})
+export const _postMove = (
+  sessionId: string,
+  rowIndex: number,
+  colIndex: number,
+  piece: PIECE
+) =>
+  axios({
+    method: "post",
+    withCredentials: true,
+    url: `${API_URL}/${sessionId}?i=${rowIndex}&j=${colIndex}&piece=${piece}`,
+  });
 
-export const _deleteSession = (sessionId: string) => axios({
-  method: 'delete',
-  withCredentials: true,
-  url: `${API_URL}/${sessionId}`
-})
+export const _deleteSession = (sessionId: string) =>
+  axios({
+    method: "delete",
+    withCredentials: true,
+    url: `${API_URL}/${sessionId}`,
+  });
